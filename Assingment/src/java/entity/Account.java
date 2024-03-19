a232bbacc6939e8935d7df3f0157ccf99b97a889
Anhnt090104 <nta.work03@gmail.com>
2024-03-19 21:53:53 +0700

    Entity
    add entity

---
+++ Assingment/src/java/entity/Account.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package entity;
+
+/**
+ *
+ * @author sonnt
+ */
+public class Account {
+    private String username;
+    private String password;
+    private String displayname;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getDisplayname() {
+        return displayname;
+    }
+
+    public void setDisplayname(String displayname) {
+        this.displayname = displayname;
+    }
+
+    @Override
+    public String toString() {
+        return "Account{" + "username=" + username + ", password=" + password + ", displayname=" + displayname + '}';
+    }
+    
+}
